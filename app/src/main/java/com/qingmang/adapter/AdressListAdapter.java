79f10909c6262ddbb0e8d6e6aaa6b50a7c2d59c4
cornflower <861281331@qq.com>
2018-03-21 22:43:59 +0800

    确认订单页面

---
+++ app/src/main/java/com/qingmang/adapter/AdressListAdapter.java
@@ -0,0 +1,32 @@
+package com.qingmang.adapter;
+
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.qingmang.R;
+import com.qingmang.moudle.entity.Message;
+
+import java.util.List;
+
+/**
+ * Created by xiejingbao on 2018/3/9.
+ */
+
+public class AdressListAdapter extends BaseQuickAdapter<Message.DataBean.ContentBean, BaseViewHolder> {
+
+
+    public AdressListAdapter(@Nullable List<Message.DataBean.ContentBean> data) {
+        super(R.layout.adress_item, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Message.DataBean.ContentBean item) {
+//        helper.setText(R.id.tv_des,item.getContent());
+//        helper.setText(R.id.tv_type,item.getState());
+//        helper.setText(R.id.tv_date,String.valueOf(item.getCreatetime()));
+//        helper.setText(R.id.tv_place,item.getPlace());
+    }
+
+
+}
---
+++ app/src/main/java/com/qingmang/moudle/entity/Adress.java
@@ -0,0 +1,8 @@
+package com.qingmang.moudle.entity;
+
+/**
+ * Created by xiejingbao on 2018/3/21.
+ */
+
+public class Adress {
+}
