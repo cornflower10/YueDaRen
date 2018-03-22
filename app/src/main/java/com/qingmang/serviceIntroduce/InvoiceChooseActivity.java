c00a5127beaad78fa580daef09f548e54e88b474
cornflower <861281331@qq.com>
2018-03-22 23:43:13 +0800

    发票

--- app/src/main/java/com/qingmang/serviceIntroduce/InvoiceChooseActivity.java
+++ app/src/main/java/com/qingmang/serviceIntroduce/InvoiceChooseActivity.java
@@ -5,11 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.qingmang.R;
 
-public class InvoiceSettingActivity extends AppCompatActivity {
+public class InvoiceChooseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_invoice_setting2);
+        setContentView(R.layout.activity_invoice_setting);
     }
 }
